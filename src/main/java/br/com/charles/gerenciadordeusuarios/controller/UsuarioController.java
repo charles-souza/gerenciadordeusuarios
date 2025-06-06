284940c66ca1a08b190e43f2b441a2989ea1a84a
charles.souza <charl3slucas@gmail.com>
2025-06-06 14:46:00 -0300

    adicionado arquivos de code style parao e realizado a formatação de todo o projeto

--- src/main/java/br/com/charles/gerenciadordeusuarios/controller/UsuarioController.java
+++ src/main/java/br/com/charles/gerenciadordeusuarios/controller/UsuarioController.java
@@ -1,7 +1,8 @@
 package br.com.charles.gerenciadordeusuarios.controller;
 
+import br.com.charles.gerenciadordeusuarios.dto.UsuarioDTO;
+import br.com.charles.gerenciadordeusuarios.service.UsuarioService;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,37 +13,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.charles.gerenciadordeusuarios.dto.UsuarioDTO;
-import br.com.charles.gerenciadordeusuarios.service.UsuarioService;
-
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
-	private UsuarioService usuarioService;
+  private final UsuarioService usuarioService;
 
-	public UsuarioController(UsuarioService usuarioService){
-		this.usuarioService = usuarioService;
-	}
-	
-	@GetMapping
-	public List<UsuarioDTO> listarTodos() {
-		return usuarioService.listarTodos();
-	}
-	
-	@PostMapping
-	public void inserir(@RequestBody UsuarioDTO usuario) {
-		usuarioService.inserir(usuario);
-	}
-	
-	@PutMapping
-	public UsuarioDTO alterar(@RequestBody UsuarioDTO usuario) {
-		return usuarioService.alterar(usuario);
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> excluir(@PathVariable Long id){
-		usuarioService.excluir(id);
-		return ResponseEntity.ok().build();
-	}
+  public UsuarioController(UsuarioService usuarioService) {
+    this.usuarioService = usuarioService;
+  }
+
+  @GetMapping
+  public List<UsuarioDTO> listarTodos() {
+    return usuarioService.listarTodos();
+  }
+
+  @PostMapping
+  public void inserir(@RequestBody UsuarioDTO usuario) {
+    usuarioService.inserir(usuario);
+  }
+
+  @PutMapping
+  public UsuarioDTO alterar(@RequestBody UsuarioDTO usuario) {
+    return usuarioService.alterar(usuario);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> excluir(@PathVariable Long id) {
+    usuarioService.excluir(id);
+    return ResponseEntity.ok().build();
+  }
 
 }
